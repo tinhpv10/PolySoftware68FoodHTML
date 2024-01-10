1f2d6df1dc3f1c7f3afec83a1287dbb56114239b
TrieuHongKy <trieuhongky11112003@gmail.com>
2024-01-10 12:53:24 +0700

    [Style] - Add Main Style To Source

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,27 @@ module.exports = {
       './index.html',],
   theme: {
     extend: {
+      colors: {
+        // 'lightRed': 'C8151C',
+        'color-blue': '#4888C6',
+        'color-success': '#941D34',
+        'color-orange': '#F94F4F',
+        'color-text': '#686363',
+        'color-secondary': '#A39A9B',
+        'color-warning': '#E0A750',
+        'color-yellow': '#FFCA40',
+        'color-danger': '#F94F4F',
+        'color-green': '#2A770F',
+        'color-white': '#FFFFFF',
+        'color-bg-gray-light': '#F1F1F1',
+        'color-bg-gray-dark': '#D5D5D5',
+      },
       fontFamily: {
         roboto: ['Roboto', 'sans-serif']
       }
     },
   },
+  variants: {},
   plugins: [],
 }
 
